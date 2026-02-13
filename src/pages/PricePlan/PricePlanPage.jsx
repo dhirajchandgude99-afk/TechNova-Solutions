4792bf24d73ac8828d5987496a0b6bd46b9f45d4
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-02-13 19:52:52 +0530

    update priceplanpage.jsx

--- src/pages/PricePlan/PricePlanPage.jsx
+++ src/pages/PricePlan/PricePlanPage.jsx
@@ -1,7 +1,7 @@
 import Faq from "../../components/sections/faq/Faq";
 import Newslatter from "../../components/sections/newsletter/Newsletter";
 import NewslatterStyle3 from "../../components/sections/newsletter/NewsletterStyle3";
-import Pricing from "../../components/sections/pricing/Pricing";
+// import Pricing from "../../components/sections/pricing/Pricing";
 import BreadCrumb from "../../components/ui/breadCrumb/BreadCrumb";
 
 const breadcumb_data = {
