33a9c56bf90b2be871d9a2aa6054cf02b25a0d32
Dhiraj Chandgude <dhirajchandgude99@gmail.com>
2026-02-14 14:42:16 +0530

    update notfoundinfo.jsx

--- src/components/sections/404/NotFoundInfo.jsx
+++ src/components/sections/404/NotFoundInfo.jsx
@@ -7,7 +7,6 @@ export default function NotFoundInfo() {
         <div className="row">
           <div className="offset-md-2 col-md-8 offset-lg-3 col-lg-6">
             <div className="error-content text-center">
-              <img src="https://images.unsplash.com/photo-1552664730-d307ca884978?w=500&h=400&fit=crop" alt="error" className="h-100" width={400} height={300} />
               <Link to={"/"} className="thm-btn">
                 <span className="txt">Back To Home</span>
               </Link>
